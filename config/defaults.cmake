56a44e16a6fc3fe3e37843eb211e49d6661a07b2
Madison Sheridan <madison.sheridan94@gmail.com>
2022-10-10 11:27:49 -0500

    Implementing cmake for project, current working implementation of importing fortran code, updating .gitignore to ignore all cmake generated files.

---
+++ config/defaults.cmake
@@ -0,0 +1,7 @@
+set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
+set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/build/MFEMConfig.cmake)
+
+# Setting CXX/MPICXX on the command line or in user.cmake will overwrite the
+# autodetected C++ compiler.
+# set(CXX gcc)
+# set(MPICXX mpicc)

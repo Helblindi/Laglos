40519d1a14d864848b8929057788bbd2fa891c35
Madison Sheridan <madison.sheridan94@gmail.com>
2023-08-07 17:46:04 -0700

    CTest is calculating correct max wave speed for eulerian code. Must do same thing for Lagrangian and then validate using output_ref_c.

--- tests/test_fortran_module.cpp
+++ tests/test_fortran_module.cpp
@@ -1,5 +1,6 @@
 #include "mfem.hpp"
 #include "var-config.h"
+#include "stdbool.h"
 #include <cmath>
 #include <sstream>
 #include <fstream>
@@ -19,15 +20,16 @@ extern "C" {
 
 // Fortran subroutine from Eulerian code
 extern "C" {
-   void __arbitrary_eos_lambda_module_MOD_eulerian_lambda_arbitrary_eos(
+   void __arbitrary_eos_lambda_module_MOD_lambda_arbitrary_eos(
       double *in_rhol, double *in_ul, double *in_el, double *in_pl,
       double *in_rhor, double *in_ur, double *in_er, double *in_pr,
       double *in_tol, bool *no_iter,double *lambda_maxl_out,
-      double *lambda_maxr_out, double *pstar, double *vstar, int *k, double *b_covolume);
+      double *lambda_maxr_out, double *pstar, int *k, double* b_covolume);
 }
 
 int test_lagrangian_lambda_max();
 int test_eulerian_lambda_max();
+double gamma_law_internal(double b_covolume, double rho, double p, double gamma);
 
 int main()
 {
@@ -45,23 +47,87 @@ int test_eulerian_lambda_max()
 
    std::string line;
 
-   double a;
    std::string ind;
+   int num_cases = 0, case_num = 0, k = 0;
+   double rhoL, rhoR, uL, uR, pL, pR, tol;
+   double b_covolume, gamma, eL, eR;
+   double lambdaL, lambdaR, pstar, vstar;
+
+   bool next_num_cases = false;
+   bool next_initial_data = false;
+   bool next_tol = false;
+   bool no_iter = false;
+
    while (std::getline(infile, line))
    {
       std::istringstream iss(line);
       iss >> ind;
-      cout << "ind: " << ind << endl;
+      // cout << "ind: " << ind << endl;
 
-      if (iss >> a)
+      /* Check indicators */
+      if (next_num_cases)
       {
-         cout << "wrote to double\n";
+         // cout << "Grabbing num_cases\n";
+         iss >> num_cases;
+         next_num_cases = false;
+      }
+      else if (next_initial_data)
+      {
+         // cout << "Grabbing initial data for Case: " << case_num << endl;
+         // Set initial data
+         // rhol, rhor, ul, ur, pl, pr
+         rhoL = stod(ind);
+         iss >> rhoR
+             >> uL
+             >> uR
+             >> pL
+             >> pR;            
+
+         next_initial_data = false;
+         next_tol = true;
+      }
+      else if (next_tol)
+      {
+         // cout << "Grabbing tol\n";
+         // Set tol
+         tol = stod(ind);
+
+         // Run case
+         b_covolume = .1/(max(rhoL, rhoR));
+         // b_covolume = 0.;
+         gamma = 1.4;
+         eL = gamma_law_internal(b_covolume, rhoL, pL, gamma);
+         eR = gamma_law_internal(b_covolume, rhoR, pR, gamma);
+         __arbitrary_eos_lambda_module_MOD_lambda_arbitrary_eos(
+            &rhoL, &uL, &eL, &pL, &rhoR, &uR, &eR, &pR, &tol, 
+            &no_iter, &lambdaL, &lambdaR, &pstar, &k, &b_covolume);
+         
+         next_tol = false;
+
+         // Output
+         cout << "===Case " << case_num << endl
+         << "lambda_max=" << max(abs(lambdaL), abs(lambdaR)) << ", pstar=" << pstar << " k=" << k << endl;
       }
 
       // int a, b;
       // if (!(iss >> a >> b)) { break; } // error
-      cout << "string: " << iss.str() << endl;
+      // cout << "string: " << iss.str() << endl;
       // process pair (a,b)
+
+
+      /* Set indicators for next line */
+      if (ind == "===Number")
+      {
+         next_num_cases = true;
+         // cout << "we have the total num case:\n";
+      }
+      else if (ind == "===Case")
+      {
+         next_initial_data = true;
+         // cout << "we have a case\n";
+
+         iss >> case_num;
+      }
    }
 
    return 0;
@@ -85,4 +151,9 @@ int test_lagrangian_lambda_max()
    }
 
    return 0;
+}
+
+double gamma_law_internal(double b_covolume, double rho, double p, double gamma)
+{
+   return p*(1.-b_covolume*rho)/((gamma-1.)*rho);
 }

9a3adfb5890b2d5ee539d64ab6e458e8dd9351ea
Madison Sheridan <madison.sheridan94@gmail.com>
2023-11-22 16:38:04 -0600

    Modified refinement plot function to only plot density.  This generated the centerline Sod shocktube plot submitted to Guermond for the first order dg0 paper.

--- scripts/create_refinement_plot.py
+++ scripts/create_refinement_plot.py
@@ -14,34 +14,45 @@ from tabulate import tabulate
 
 
 # check command line inputs
-assert len(sys.argv) == 3, "This file needs 2 input arguments: directory, num cols, but " + str(len(sys.argv)) + " were given."
+# assert len(sys.argv) == 3, "This file needs 2 input arguments: directory, num cols, but " + str(len(sys.argv)) + " were given."
 directory = str(sys.argv[1])
-num_cols = int(sys.argv[2])
+# num_cols = int(sys.argv[2])
 
 def main():
-   fig, axs = plt.subplots(1, num_cols)
+   # fig, axs = plt.subplots(1, 1)
    for filename in sorted(os.listdir(directory)):
       f = os.path.join(directory, filename)
       refinements = filename[3:5]
+      
       print('refinements: ', refinements)
       df = pd.read_csv(f, dtype=float).sort_values(by=['x'])
 
-      for i, col in enumerate(df.columns):
-         if (i == 0): continue # Skip first column
-         axs[i-1].set_xlabel(df.columns[0])
-         axs[i-1].set_ylabel(col)
-         if (refinements == "ex"):
-            axs[i-1].plot(df[df.columns[0]], df[col], 'k--', label=refinements)
-         else:
-            axs[i-1].plot(df[df.columns[0]], df[col], '-', label=refinements)
+      # if (i == 0): continue # Skip first column
+      # plt.set_xlabel(df.columns[0])
+      # plt.set_ylabel("$\rho$")
+      if (refinements == "ex"):
+         plt.plot(df[df.columns[0]], df[df.columns[1]], 'k-', label="Exact solution")
+      else:
+         num_ref = int(refinements)
+         _label = "# dof = " + str(500 * pow(4,num_ref))
+         plt.plot(df[df.columns[0]], df[df.columns[1]], '--', label=_label)
 
       # plt.xlabel(df.columns[0])
       # plt.ylabel(df.columns[1])
       # plt.title("Sod Shocktube")
-      # if (refinements == "ex"):
-      #    plt.plot(df[df.columns[0]], df[df.columns[1]], 'k--', label=refinements)
-      # else:
-      #    plt.plot(df[df.columns[0]], df[df.columns[1]], '-', label=refinements)
+
+      # for i, col in enumerate(df.columns):
+      #    if (i == 0): continue # Skip first column
+      #    axs[i-1].set_xlabel(df.columns[0])
+      #    axs[i-1].set_ylabel(col)
+      #    if (refinements == "ex"):
+      #       axs[i-1].plot(df[df.columns[0]], df[col], 'k--', label=refinements)
+      #    else:
+      #       axs[i-1].plot(df[df.columns[0]], df[col], '-', label=refinements)
+
+      # plt.xlabel(df.columns[0])
+      # plt.ylabel(df.columns[1])
+      # plt.title("Sod Shocktube")
    
    plt.legend()
    plt.show()

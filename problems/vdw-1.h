f84d694d0aa7bd1cc054f5d49a7da8a72b114d8d
Madison Sheridan <madison.sheridan94@gmail.com>
2023-11-30 15:38:23 -0600

    Added an indicator for Vdw1.

--- problems/vdw-1.h
+++ problems/vdw-1.h
@@ -42,6 +42,8 @@ private:
    double _a = 1., _b = 1., _gamma = 1.02;
    bool distort_mesh = false;
    bool known_exact_solution = true; // exact is known, information in supplementary material
+   bool bcs = false; // Indicator for boundary conditions
+   string _indicator = "Vdw1"; // Possible: saltzmann
 
    double rhop = 0.35, x0 = 0.;
    double rhoL = 0.1, rhoR = 0.39;
@@ -58,6 +60,7 @@ public:
       this->set_a(_a);
       this->set_b(_b);
       this->set_gamma(_gamma);
+      this->set_indicator(_indicator);
    }
 
    /* Override getters */

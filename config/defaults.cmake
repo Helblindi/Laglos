a65e093831bfa82fd4b75a56fd17d686a9ca244f
Madison Sheridan <madison.sheridan94@gmail.com>
2024-09-20 15:38:31 -0500

    Adding in configuration file to set options and directory locations.

--- config/defaults.cmake
+++ config/defaults.cmake
@@ -1,7 +1,8 @@
 set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
 set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
 
-# Setting CXX/MPICXX on the command line or in user.cmake will overwrite the
-# autodetected C++ compiler.
-# set(CXX gcc)
-# set(MPICXX mpicc)
+set(LAGLOS_COINHSL_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../coinhsl/)
+set(LAGLOS_HIOP_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../hiop/)
+
+option(LAGLOS_USE_HIOP "Utilize sparse linear algebra" ON)
+option(LAGLOS_USE_COINHSL "Use COINHSL for sparse linear algebra" ON)

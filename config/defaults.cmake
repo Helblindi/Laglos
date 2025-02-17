bca23553f9721deaef782d61bb0b25c79c1f14f1
Madison Sheridan <madison.sheridan94@gmail.com>
2025-02-17 17:35:36 -0600

    For some reason I cannot get find_dependency() to autopopulate in the LAGLOSConfig.cmake file for all required packages from Laglos. This means that cmake packages that import the Laglos library will need to also import it's required packages.

--- config/defaults.cmake
+++ config/defaults.cmake
@@ -4,3 +4,4 @@ set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
 set(LAGLOS_HIOP_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../hiop/)
 
 option(LAGLOS_USE_HIOP "Utilize sparse linear algebra" ON)
+option(LAGLOS_USE_BOOST "Utilize sparse linear algebra" ON)

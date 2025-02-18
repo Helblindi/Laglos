da6c83ccdb71582f4e4ffad356b92d71f268af65
Madison Sheridan <madison.sheridan94@gmail.com>
2025-02-18 11:09:28 -0600

    Adjusted cmake variables pointing to mfem_DIR and MFEM_DIR to represent install directory location and config file location. Only load hiop configuration if the parameter is set to on.

--- config/defaults.cmake
+++ config/defaults.cmake
@@ -1,7 +1,9 @@
-set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
-set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
+set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/build/install
+    CACHE PATH "absolute path to the MFEM build or install prefix")
+set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/
+    CACHE PATH "absolute path to where MFEMConfig.cmake is")
 
 set(LAGLOS_HIOP_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../hiop/)
 
-option(LAGLOS_USE_HIOP "Utilize sparse linear algebra" ON)
-option(LAGLOS_USE_BOOST "Utilize sparse linear algebra" ON)
+option(LAGLOS_USE_HIOP "Utilize sparse linear algebra" OFF)
+option(LAGLOS_USE_BOOST "Utilize BOOST" ON)

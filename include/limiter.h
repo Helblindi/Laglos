db34d95459faafbf54703fd32fb60023d50f22da
Madison Sheridan <madison.sheridan94@gmail.com>
2025-04-01 12:26:48 -0500

    Implemented HO adjacency retrieval function and max/min limiting.  Stoppage criteria is no change made in either max/min limiting iterate or if max iterations of 5 have been reached.  Notably, sometimes the first criteria is reached meaning not all DOFs have been limited.  In practice this happends for a minimal number of DoFs.

--- include/limiter.h
+++ include/limiter.h
@@ -72,6 +72,10 @@ private:
    ParDiscreteLinearOperator vert_elem_oper; 
    HypreParMatrix *vert_elem, *vert_elem_trans;
 
+   SparseMatrix _spm, _spm_trans;
+
+   const int max_it = 5;
+
 public:
 IDPLimiter(ParFiniteElementSpace & _pfes, ParFiniteElementSpace & H1FESpace_proj_LO,
            ParFiniteElementSpace & H1FESpace_proj_HO, const HypreParVector &_mass_vec) :
@@ -93,20 +97,15 @@ IDPLimiter(ParFiniteElementSpace & _pfes, ParFiniteElementSpace & H1FESpace_proj
    }
 
    // Try @ https://github.com/mfem/mfem/issues/436
+   // _spm & _spm_trans will be used to determine adjacency
    vert_elem_oper.AddDomainInterpolator(new IdentityInterpolatorMax);
    vert_elem_oper.Assemble();
    vert_elem_oper.Finalize();
    vert_elem = vert_elem_oper.ParallelAssemble();
    vert_elem_trans = vert_elem->Transpose();
 
-   SparseMatrix _spm, _spm_trans;
    vert_elem->MergeDiagAndOffd(_spm);
    vert_elem_trans->MergeDiagAndOffd(_spm_trans);
-   cout << "vert_elem: ";
-   _spm.Print(cout);
-   cout << "transpose: ";
-   _spm_trans.Print(cout);
-   // cout << "height: " << _spm.Height() << ", width: " << _spm.Width() << endl;
 }
 
 ~IDPLimiter() 
@@ -115,22 +114,74 @@ IDPLimiter(ParFiniteElementSpace & _pfes, ParFiniteElementSpace & H1FESpace_proj
    delete vert_elem;
 }
 
+
 /**
- * @brief Given a low order and high order representation of a thermodynamical variables,
- * perform local mass conservative limiting on the high order solution using the low 
- * order IDP update.
- *
- * @param gf_lo the invariant-domain-preserving low order update
- * @param gf_ho the high order update, not yet IDP
+ * @brief Applies a local conservative limiting process to ensure the high-order 
+ *        solution remains within specified bounds.
+ * 
+ * This function iteratively adjusts the high-order solution (`gf_ho`) to ensure 
+ * it satisfies local conservation properties while staying within the minimum 
+ * and maximum bounds computed from the low-order solution (`gf_lo`). The process 
+ * continues until no further adjustments are needed or a maximum number of 
+ * iterations is reached.
+ * 
+ * @param gf_lo The low-order solution used to compute the minimum and maximum bounds.
+ * @param gf_ho The high-order solution to be limited. This parameter is modified in-place.
+ * 
+ * The function performs the following steps:
+ * 1. Computes the minimum and maximum bounds (`rho_min` and `rho_max`) from `gf_lo`.
+ * 2. Iteratively adjusts `gf_ho` using `LimitMassMax` and `LimitMassMin` to ensure 
+ *    it stays within the bounds.
+ * 3. Stops iterating when no further adjustments are made or the maximum number 
+ *    of iterations (`max_it`) is reached.
+ * 
+ * Debugging output is provided to indicate the percentage of the solution that 
+ * was limited in each iteration. Uncommented sections of the code can be used 
+ * for additional debugging to verify that all degrees of freedom (DoFs) in `gf_ho` 
+ * remain within the computed bounds.
  */
 void LocalConservativeLimit(const ParGridFunction &gf_lo, ParGridFunction &gf_ho)
 {
    // cout << "IDPLimiter::LocalConservativeLimit\n";
    ComputeRhoMinMax(gf_lo);
 
-   MFEM_WARNING("Possible iterations.\n");
-   LimitMassMax(gf_ho);
-   LimitMassMin(gf_ho);
+   double pct_max_lim = 1., pct_min_lim = 1.;
+   double pct_max_lim_old = 0., pct_min_lim_old = 0.;
+   int num_it = 0;
+   bool max_it_reached = false, done_iterating = false;
+   while (!done_iterating && !max_it_reached)
+   {
+      pct_min_lim_old = pct_min_lim;
+      pct_max_lim_old = pct_max_lim;
+
+      LimitMassMax(gf_ho, pct_max_lim);
+      LimitMassMin(gf_ho, pct_min_lim);
+
+      /* Check if any adjustment has been made */
+      if (pct_min_lim == pct_min_lim_old && pct_max_lim == pct_max_lim_old)
+      {
+         /* No improvement from last iteratior */
+         done_iterating = true;
+      }
+
+      /* Check for max it reached */
+      num_it++;
+      if (num_it == max_it) { max_it_reached = true;}
+
+      // if(pct_max_lim + pct_min_lim > 0.)
+      // {
+      //    cout << "pct max limited: " << pct_max_lim << endl;
+      //    cout << "pct min limited: " << pct_min_lim << endl;
+      // }
+   }
+   // for (int i = 0; i < NDofs; i++)
+   // {
+   //    if (gf_ho[i] < rho_min[i] || gf_ho[i] > rho_max[i])
+   //    {
+   //       cout << "dof: " << i << ", min: " << rho_min[i] << ", ho: " << gf_ho[i] << ", max: " << rho_max[i] << endl;
+   //    }
+   // }
+   
 }
 
 /**
@@ -159,9 +210,58 @@ void ComputeRhoMinMax(const ParGridFunction &gf_lo)
    vert_elem->Mult(LO_proj_min, rho_min);
 }
 
+
+/**
+ * @brief Retrieves the adjacency information for a given degree of freedom (DOF).
+ *
+ * This function populates the provided array with the adjacent LO degrees of freedom
+ * (DOFs) for a given input HO DOF. 
+ *
+ * @param dof The HO degree of freedom (DOF) for which adjacency information is required.
+ * @param adj_dofs The set of HO degrees of freedom (DOF) that dof can exchange mass with.
+ *
+ *
+ * Example:
+ *
+ *    *-----*-----*
+ *    |6   7|10 11|
+ *    |     |     |
+ *    |4   5|8   9|
+ *    *-----*-----*
+ *    |2   3|14 15|
+ *    |     |     |
+ *    |0   1|12 13|
+ *    *-----*-----*
+ *
+ *    ------------------------
+ *    | dof | adj_dofs       |
+ *    |  5  | 3,4,5,6,7,8,14 |
+ *    |  13 | 12,13,14,15    |
+ *    |  4  | 2,4,5,6,7      |
+ *    |  10 | 7.8.9.10,11    |
+ *    ------------------------ 
+ */
 void GetAdjacency(const int dof, Array<int> &adj_dofs)
 {
-   MFEM_ABORT("How to collect eligible neighbors?");
+   // cout << "----------IDPLimiter::GetAdjacency----------\n";
+
+   /* first retrieve HO dofs that share the same max/min given by the high order space */
+   Vector LO_vals, adj_vals; // vector isn't used
+   Array<int> LO_verts;
+   _spm.GetRow(dof, LO_verts, LO_vals); // val isn't used
+   assert(LO_verts.Size() == 1); // Each HO dof should only correspond to one LO H1 dof
+   _spm_trans.GetRow(LO_verts[0], adj_dofs, adj_vals);
+   assert((adj_dofs.Size() <= 4) && "Cannot have more than 4 values sharing a vertex.");
+
+   /* Next, append HO dofs that are in the same mesh element */
+   int el_index = pfes.GetElementForDof(dof);
+   Array<int> el_dofs;
+   pfes.GetElementDofs(el_index, el_dofs);
+   adj_dofs.Append(el_dofs);
+
+   /* Remove dof from adj_dofs */
+   adj_dofs.DeleteFirst(dof);
+   adj_dofs.DeleteFirst(dof);
 }
 
 /**
@@ -171,8 +271,9 @@ void GetAdjacency(const int dof, Array<int> &adj_dofs)
  *
  * @param rho_gf_ho the high order update, not yet IDP
  */
-void LimitMassMax(ParGridFunction &gf_ho)
+void LimitMassMax(ParGridFunction &gf_ho, double &pct_limited)
 {
+   int num_limited = 0;
    // cout << "IDPLimiter::LimitMassMax\n";
    if (gf_ho.Size() != NDofs)
    {
@@ -184,6 +285,13 @@ void LimitMassMax(ParGridFunction &gf_ho)
    /* Iterate over DoFs */
    for (int dof_it = 0; dof_it < NDofs; dof_it++)
    {
+      if (gf_ho[dof_it] <= rho_max[dof_it])
+      {
+         /* Maximum principle is satisfied */
+         continue;
+      }
+      num_limited++;
+
       /* Get adjacency */
       GetAdjacency(dof_it, adj_dofs);
 
@@ -204,6 +312,10 @@ void LimitMassMax(ParGridFunction &gf_ho)
       {
          lip = mass_vec[dof_it] * (gf_ho[dof_it] - bip) / aip;
       }
+      else if (aip < 0.)
+      {
+         MFEM_ABORT("aim <= 0.");
+      }
 
       /* Set y_i */
       gf_ho[dof_it] = bip;
@@ -215,6 +327,7 @@ void LimitMassMax(ParGridFunction &gf_ho)
          gf_ho[dof_j] = gf_ho[dof_j] + lip * std::max(0., rho_max[dof_j] - gf_ho[dof_j]);
       }
    }
+   pct_limited = double(num_limited) / NDofs;
 }
 
 /**
@@ -224,9 +337,63 @@ void LimitMassMax(ParGridFunction &gf_ho)
  *
  * @param rho_gf_ho the high order update, not yet IDP
  */
-void LimitMassMin(ParGridFunction &gf_ho)
+void LimitMassMin(ParGridFunction &gf_ho, double &pct_limited)
 {
-   
+   int num_limited = 0;
+   // cout << "IDPLimiter::LimitMassMax\n";
+   if (gf_ho.Size() != NDofs)
+   {
+      MFEM_ABORT("Incompatible sizes of gridfunctions provided.\n");
+   }
+
+   Array<int> adj_dofs;
+
+   /* Iterate over DoFs */
+   for (int dof_it = 0; dof_it < NDofs; dof_it++)
+   {
+      if (gf_ho[dof_it] >= rho_min[dof_it])
+      {
+         /* Maximum principle is satisfied */
+         continue;
+      }
+      num_limited++;
+
+      /* Get adjacency */
+      GetAdjacency(dof_it, adj_dofs);
+
+      /* Compute ai- */
+      double aim = 0.;
+      for (int j = 0; j < adj_dofs.Size(); j++)
+      {
+         int dof_j = adj_dofs[j];
+         aim += mass_vec[dof_j]*std::max(0., gf_ho[dof_j] - rho_min[dof_j]);
+      }
+
+      /* Compute bi+ */
+      double bim = min(gf_ho[dof_it] + aim / mass_vec[dof_it], rho_min[dof_it]);
+
+      /* Compute li+ */
+      double lim = 0.;
+      if (aim > 0.)
+      {
+         lim = mass_vec[dof_it] * (gf_ho[dof_it] - bim) / aim;
+      }
+      else if (aim < 0.)
+      {
+         MFEM_ABORT("aim <= 0.");
+      }
+
+      /* Set y_i */
+      gf_ho[dof_it] = bim;
+
+      /* Set y_j for all neighbors */
+      for (int j = 0; j < adj_dofs.Size(); j++)
+      {
+         int dof_j = adj_dofs[j];
+         gf_ho[dof_j] = gf_ho[dof_j] + lim * std::max(0., gf_ho[dof_j] - rho_max[dof_j]);
+      }
+   }
+   pct_limited =  double(num_limited) / NDofs;
 }
    
 }; // IDPLimiter

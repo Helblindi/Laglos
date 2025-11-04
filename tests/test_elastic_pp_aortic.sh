e53c6d096d642c97bca32e0ef00f1c1966e68882
Madison Sheridan <madison.sheridan94@gmail.com>
2025-11-04 10:05:17 -0600

    Added test for elastic projectile plate using the aortic model with theta = pi / 4.

---
+++ tests/test_elastic_pp_aortic.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+SOURCE_DIR="$1"
+OUTPUT_FILE="$2"
+
+# Full path to the executable
+EXECUTABLE="${SOURCE_DIR}/build/Laglos"
+
+# Check that the executable exists
+if [[ ! -x "$EXECUTABLE" ]]; then
+    echo "Executable not found or not executable: $EXECUTABLE"
+    exit 1
+fi
+
+# test parameters
+mesh_file="${SOURCE_DIR}/data/elastic/projectile-plate.mesh"
+problem=54
+final_time=.000103
+cfl=0.5
+mv_option=2
+refinements_serial=1
+
+options="-m ${mesh_file} -p ${problem} "
+options+="-tf ${final_time} -cfl ${cfl} "
+options+="-mv ${mv_option} -ppd "
+options+="-rs ${refinements_serial} "
+options+="-ue 3 " # Aortic
+echo $options
+
+# Run the executable with input file (adjust args as needed)
+$EXECUTABLE $options > "${OUTPUT_FILE}"
+

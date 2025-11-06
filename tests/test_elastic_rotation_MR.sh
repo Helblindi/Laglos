6401fc9b2afb645201954a04b17a75329988876d
Madison Sheridan <madison.sheridan94@gmail.com>
2025-11-06 11:27:35 -0600

    Adding test for elastic rotation using MR. Changing test file for elastic impact to denote it is a NH test.

---
+++ tests/test_elastic_rotation_MR.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+SOURCE_DIR="$1"
+OUTPUT_FILE="$2"
+
+# Full path to the executable
+EXECUTABLE="${SOURCE_DIR}/build/Laglos"
+
+# Check that the executable exists
+if [[ ! -x "$EXECUTABLE" ]]; then
+    echo "Executable not found or not executable: $EXECUTABLE"
+    exit 1
+fi
+
+# test parameters
+mesh_file="${SOURCE_DIR}/data/elastic/ref-square-c0-p15.mesh"
+problem=57
+final_time=.00001
+cfl=0.5
+mv_option=2
+refinements_serial=4
+
+options="-m ${mesh_file} -p ${problem} "
+options+="-tf ${final_time} -cfl ${cfl} "
+options+="-mv ${mv_option} "
+options+="-rs ${refinements_serial} "
+options+="-ue 2 " # Mooney-Rivlin
+echo $options
+
+# Run the executable with input file (adjust args as needed)
+$EXECUTABLE $options > "${OUTPUT_FILE}"
+

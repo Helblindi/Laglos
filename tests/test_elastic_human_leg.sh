88ef15f3765683b2630acfbdc238ed5d403391cb
Madison Sheridan <madison.sheridan94@gmail.com>
2025-11-20 13:44:37 -0600

    For elastic human leg test, only run first 100 steps.

--- tests/test_elastic_human_leg.sh
+++ tests/test_elastic_human_leg.sh
@@ -17,6 +17,7 @@ mesh_file="${SOURCE_DIR}/data/elastic/human_leg_impact.mesh"
 problem=53
 final_time=.0012
 cfl=0.5
+max_steps=100
 mv_option=2
 refinements_serial=0
 
@@ -24,6 +25,7 @@ options="-m ${mesh_file} -p ${problem} "
 options+="-tf ${final_time} -cfl ${cfl} "
 options+="-mv ${mv_option} -ppd "
 options+="-rs ${refinements_serial} "
+options+="-ms ${max_steps} "
 echo $options
 
 # Run the executable with input file (adjust args as needed)
